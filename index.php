a90a3f847dff84493940a140470ad20c6ffe0475
gustavo <gustavohks2@gmail.com>
2018-01-25 10:55:38 -0200

    Add basic workflow and setup

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
-<?php
-  echo "Hello World";
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="robots" content="follow">
+    <meta name="author" content="Gustavo Henrique">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="PHP login system project">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Home</title>
+  </head>
+  <body>
+    
+  </body>
+</html>
