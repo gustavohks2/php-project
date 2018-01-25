7b51e9beaa4282da69552be4cc602ad4e8919857
gustavo <gustavohks2@gmail.com>
2018-01-25 12:35:53 -0200

    Create nav and header HTML structure

--- index.php
+++ index.php
@@ -13,6 +13,35 @@
     <title>Home</title>
   </head>
   <body>
+    <div class="navigation">
+      <div class="navigation__logo-box">
+        <i class="navigation__logo"></i>
+      </div>
+      <h3 class="navigation__heading">Dev Solutions</h3>
+      <form class="navigation__form">
+        <div class="navigation__form-group">
+          <label class="navigation__label" for="username">Username</label>
+          <input id="username" type="text" class="navigation__input" required>
+        </div>
+        <div class="navigation__form-group">
+          <label class="navigation__label" for="password">Password</label>
+          <input id="password" type="password" class="navigation__input" required>
+        </div>
+        <button class="btn btn--rounded btn--color" type="submit">Login</button>
+      </form>
+    </div>
     
+    <header class="header">
+      <div class="header__text-box">
+        <div class="header__logo-box">
+          <img src="img/dev-solutions-logo.jpg" alt="Dev Solutions logo">
+        </div>
+        <div class="heading-primary">
+            <h1 class="heading-primary--main">Dev Solutions</h1>
+            <h2 class="heading-primary--sub">Sign up today, and discover awesome things!</h2>
+        </div>
+        <button class="btn btn--rounded btn--blue">Sign up</button>
+      </div>
+    </header>
   </body>
 </html>
